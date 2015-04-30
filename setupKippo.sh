6e5c5622ceccd197647848f9fb57fc26f3b9f5f7
Liso Gallo <liso@riseup.net>
2015-04-29 22:54:49 -0300

    [ADD] ccze to system packages

--- setupKippo.sh
+++ setupKippo.sh
@@ -5,7 +5,7 @@ aptitude update
 aptitude -y safe-upgrade
 
 # Install required packages
-aptitude -y install build-essential python-dev libmysqlclient-dev python-pip git
+aptitude -y install build-essential python-dev libmysqlclient-dev python-pip git ccze
 
 # Install 'iptables-persistent' package (optional)
 export DEBIAN_FRONTEND=noninteractive
